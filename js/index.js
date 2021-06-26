52d8b83454c5ab94dd3fbe0953a3969ead66831d
Shrey Kumar <shrey-kumar@Shreys-MacBook-Air.local>
2021-06-26 10:44:37 +0530

    added task to list

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
 console.log('loaded');
+
+function addTask() {
+    console.log('adding task');
+    var li = document.createElement("li");
+    var taskName = document.getElementById("taskName").value;
+    var textNode = document.createTextNode(taskName);
+    li.appendChild(textNode);
+    if (taskName === '') {
+        alert("You must write something!");
+    } else {
+        document.getElementById("allTasks").appendChild(li);
+    }
+    document.getElementById("taskName").value = "";
+
+    var span = document.createElement("span");
+    var txt = document.createTextNode("");
+}
