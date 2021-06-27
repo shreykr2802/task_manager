f20219d81f74a87f26982972d35e80487337773e
Shrey Kumar <shrey-kumar@Shreys-MacBook-Air.local>
2021-06-27 23:15:06 +0530

    Code Refactoring and Readme file added

---
+++ README.md
@@ -0,0 +1,33 @@
+# Task Manager App with simple HTML CSS and JS
+
+A project for managing Task with multiple list and multiple items under list with just plain old vanilla Javascript, HTML and some CSS.
+
+<!-- # Poject Link
+
+<a href="http://shreykr.com/spacex-visualization/">SpaceX Data Visualization</a> -->
+
+# Project Status
+
+Project is completed.
+Users can add multiple list under task and multiple item in an list.
+Users can perform various operations upon it like
+<ol>
+    <li>Add N number of list</li>
+    <li>Delete a List</li>
+    <li>Add items to List</li>
+    <li>Delete Items from the list</li>
+    <li>Edit Item description</li>
+    <li>Drag and Drop items between list</li>
+<ol>
+
+# Installation and Setup Instructions
+
+To begin with you can fork the repository.
+Clone it into Local Repository.
+No need of any server to run it, just double click `index.html` file.
+
+
+# Additional Resources used
+
+<a href="https://fonts.google.com/">Google Fonts</a> - For Fonts
+<a href="https://fontawesome.com/">Font Awesome</a> - For Icons
--- js/index.js
+++ js/index.js
@@ -1,76 +1,107 @@
-console.log('loaded');
-let i = 0;
-function addList() {
-    console.log('adding list');
+function clearErrorText() {
     const error = document.getElementById('noText');
     error.innerHTML = '';
-    let li = document.createElement("li");
-    let taskListName = document.getElementById("taskListName").value;
-    let textNode = document.createTextNode(taskListName);
-    let div = document.createElement('div');
+}
+
+function addTaskList(taskListName, li, parentId) {
+    if (taskListName.value === '') {
+        const error = document.getElementById('noText');
+        const errorText = document.createTextNode("Please provide a Name");
+        error.appendChild(errorText);
+    } else {
+        document.getElementById(parentId).appendChild(li);
+        taskListName.value = "";
+    }
+}
+
+function createAddTaskDiv(ul) {
+    const div = document.createElement('div');
     div.className = "addTaskNameContainer";
-    let inputTaskName = document.createElement('input');
+    const inputTaskName = document.createElement('input');
     inputTaskName.placeholder = "Add Task";
-    inputTaskName.id = "taskName" + i;
-    let ul = document.createElement('ul');
-    ul.id = "allTaskName" + i;
-    let addTaskButton = document.createElement('button');
+    inputTaskName.id = "taskName" + Math.random() * 2389239;
+    const addTaskButton = document.createElement('button');
     addTaskButton.appendChild(document.createTextNode('Add'));
-    addTaskButton.addEventListener("click", () => addTask(inputTaskName.id, ul.id), false);
+    addTaskButton.addEventListener("click", () => addTask(inputTaskName, ul.id), false);
     addTaskButton.id = "addTaskButton";
-    i++;
+    div.appendChild(inputTaskName);
+    div.appendChild(addTaskButton);
+    return div;
+}
+
+function createTitleDiv(textNode, li) {
+    const del = createDeleteButton(li);
+    const titlediv = document.createElement('div');
+    titlediv.style = "width: 100%; padding: 0.1rem"
+    titlediv.appendChild(textNode);
+    titlediv.appendChild(del);
+    return titlediv;
+}
+
+function addList() {
+    clearErrorText();
+    const li = document.createElement("li");
+    const taskListName = document.getElementById("taskListName");
+    const textNode = document.createTextNode(taskListName.value.toUpperCase());
+    const ul = document.createElement('ul');
+    ul.id = "allTaskName" + Math.random() * 327382739;
+    const div = createAddTaskDiv(ul);
     li.addEventListener("drop", drop, false);
     li.addEventListener("dragover", allowDrop, false);
     div.className = "addTaskDiv";
     li.className = "toDoList";
-    div.appendChild(inputTaskName);
-    div.appendChild(addTaskButton);
-    li.appendChild(textNode);
+    const titleDiv = createTitleDiv(textNode, li);
+    li.appendChild(titleDiv);
     li.appendChild(div);
     li.appendChild(ul);
-
-    if (taskListName === '') {
-        const errorText = document.createTextNode("Please provide a task List Name");
-        error.appendChild(errorText);
-    } else {
-        document.getElementById("allTasksList").appendChild(li);
-    }
-    document.getElementById("taskListName").value = "";
+    addTaskList(taskListName, li, "allTasksList");
 }
 
-function addTask(inputId, ulId) {
-    let li = document.createElement("li");
-    let taskName = document.getElementById(inputId).value;
-    let textNode = document.createElement('textarea');
-    textNode.setAttribute("id", Math.random() * 3267832);
-    textNode.value = taskName;
-    textNode.style = "padding: 0.1rem; min-width: 75%; max-width: 75%; min-height: 1.5rem; margin: 0";
-    textNode.disabled = true;
-    li.draggable = true;
-    li.id = Math.random() * 1000;
-    let edit = document.createElement('i');
+function createEditButton(textNode) {
+    const edit = document.createElement('i');
     edit.className = "far fa-edit";
-    edit.style = "color: orange; float: right; margin-right: 2px"
-    let del = document.createElement('i');
+    edit.style = "color: orange; float: right; margin-right: 2px";
+    edit.addEventListener("click", () => editTask(textNode, edit), false);
+    return edit;
+}
+
+function createDeleteButton(li) {
+    const del = document.createElement('i');
     del.className = "far fa-trash-alt";
     del.style = "color: red;float: right;"
+    del.addEventListener("click", () => deleteTask(li), false);
+    return del;
+}
+
+function createTextArea(inputName) {
+    const taskName = inputName.value;
+    const textArea = document.createElement('textarea');
+    textArea.setAttribute("id", Math.random() * 3267832);
+    textArea.value = taskName;
+    textArea.style = "padding: 0.1rem; min-width: 75%; max-width: 75%; min-height: 1.5rem; margin: 0";
+    textArea.disabled = true;
+    return textArea;
+}
+
+function addTask(inputName, ulId) {
+    clearErrorText();
+    const li = document.createElement("li");
+    li.draggable = true;
+    li.id = Math.random() * 1000;
+    const textNode = createTextArea(inputName);
+    const edit = createEditButton(textNode);
+    const del = createDeleteButton(li);
     li.addEventListener("dragstart", drag, false);
     li.appendChild(textNode);
     li.appendChild(del);
     li.appendChild(edit);
     li.className = "taskNameListItem";
-    del.addEventListener("click", () => deleteTask(li, ulId), false);
-    edit.addEventListener("click", () => editTask(textNode, edit), false);
-    if (taskName === '') {
-        alert("Please provide a task name");
-    } else {
-        document.getElementById(ulId).appendChild(li);
-    }
-    document.getElementById(inputId).value = "";
+    addTaskList(inputName, li, ulId);
+    inputName.value = "";
 }
 
-function deleteTask(taskId, listId) {
-    let ul = document.getElementById(listId);
+function deleteTask(taskId) {
+    let ul = taskId.parentNode;
     ul.removeChild(taskId);
 }
 
@@ -81,7 +112,6 @@ function editTask(textNode, edit) {
         edit.className = 'far fa-save';
     }
     textNode.disabled = !textNode.disabled;
-
 }
 
 function drag(ev) {
